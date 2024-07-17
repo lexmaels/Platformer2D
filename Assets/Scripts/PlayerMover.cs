e5fc1cb5acdc94a6bdf99896f98b28ff683e6940
lexmaels <lexmaels@gmail.com>
2024-07-18 04:57:50 +1000

    Update

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     private const float _speedCoef = 50;
+    private const string _horizaontalAxis = "Horizontal";
+
     [SerializeField] private float _speedX = 1f;
+    
+
     private Rigidbody2D _rigidbody;
+    private float _direction;
+    private string _directionMove;
     
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        _direction = Input.GetAxis(_horizaontalAxis);
+        if(_direction > 0)
+        {
+            _directionMove = "вправо";
+            Debug.Log("направление движения: " + _directionMove);
+        }
+        else if (_direction < 0)
+        {
+            _directionMove = "влево";
+            Debug.Log("направление движения: " + _directionMove);
+        }
+    }
+
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_speedX * Time.fixedDeltaTime * _speedCoef, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_speedX * _direction  * _speedCoef * Time.fixedDeltaTime, _rigidbody.velocity.y);
     }
 }
