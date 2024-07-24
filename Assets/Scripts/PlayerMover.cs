5fcb76fdbd9646d2750aad388e9b76b5a34a2da5
lexmaels <lexmaels@gmail.com>
2024-07-24 10:17:49 +1000

    Update

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -3,15 +3,17 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMover : MonoBehaviour
 {
-    private const float _speedCoef = 50;
-    private const string _horizaontalAxis = "Horizontal";
+    private const float SPEED_COEF = 50;
+    private const string HORIZONATAL_AXIS = "Horizontal";
+    private const string GROUND_TAG = "Ground";
 
-    [SerializeField] private float _speedX = 1f;
-    
+    [SerializeField] private float _speedX = 1;
+    [SerializeField] private float _jumpForce = 500;
 
     private Rigidbody2D _rigidbody;
     private float _direction;
-    private string _directionMove;
+    private bool _isJump;
+    private bool _isGround;
     
     private void Start()
     {
@@ -20,21 +22,29 @@ public class PlayerMover : MonoBehaviour
 
     private void Update()
     {
-        _direction = Input.GetAxis(_horizaontalAxis);
-        if(_direction > 0)
-        {
-            _directionMove = "вправо";
-            Debug.Log("направление движения: " + _directionMove);
-        }
-        else if (_direction < 0)
-        {
-            _directionMove = "влево";
-            Debug.Log("направление движения: " + _directionMove);
-        }
+        _direction = Input.GetAxis(HORIZONATAL_AXIS);
+
+        if (_isGround && Input.GetKeyDown(KeyCode.W))
+            _isJump = true;
     }
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = new Vector2(_speedX * _direction  * _speedCoef * Time.fixedDeltaTime, _rigidbody.velocity.y);
+        _rigidbody.velocity = new Vector2(_speedX * _direction  * SPEED_COEF * Time.fixedDeltaTime, _rigidbody.velocity.y);
+
+        if (_isJump)
+        {
+            _rigidbody.AddForce(new Vector2(0, _jumpForce));
+            _isJump = false;
+            _isGround = false;
+        }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+            _isGround = true;
+    }
+
+
 }
